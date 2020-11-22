0e54972dafa7243f4b6671bba47d7732390ba3b9
Yeuni <yeunidev@gmail.com>
2020-11-22 21:36:13 +0300

    Fix Adapter

--- app/src/main/java/www/yeuni/yeunidiceroll/MainActivity.kt
+++ app/src/main/java/www/yeuni/yeunidiceroll/MainActivity.kt
@@ -2,10 +2,20 @@ package www.yeuni.yeunidiceroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+yeuniRoll()
+
+    }
+    fun yeuniRoll(){
+
+        val diceRange = 1..6
+        val randomNumber = diceRange.random()
+        println("Random number: ${randomNumber}")
+        Log.e("davies90","Random number: "+randomNumber)
     }
 }
